8c86166f3ff29b272dc90554223d2abb8cd8592c
Arpit Lekhwar <arpit.lekhwar19@gmail.com>
2025-09-13 17:50:15 +0530

    Created Java\Strings\Exchange.java and Updated Java\Arrays\One.java

--- Java/Arrays/One.java
+++ Java/Arrays/One.java
@@ -1,8 +1,8 @@
 package Java.Arrays;
-
 import java.util.*;
 class One {
     public static void main(String[] args) {
+        int arr[][] = new int[9][9];
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         System.out.println(a);
